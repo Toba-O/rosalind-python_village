3addb4096604dbe8bf2c9b7c193ff2889545fd52
Toba Ojo <71614914+Toba-O@users.noreply.github.com>
2020-12-18 00:52:40 -0500

    Update INI1 - Installing Python.py

--- INI1 - Installing Python.py
+++ INI1 - Installing Python.py
@@ -1,3 +1,5 @@
+# INI1 Installing Python
+
 # Problem
 # After downloading and installing Python, type import this into the Python command line and see what happens. 
 # Then, click the "Download dataset" button below and copy the Zen of Python into the space provided.
