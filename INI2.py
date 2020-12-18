1cd29c336f395bc23020f652cac4f23065d795ad
Toba Ojo <71614914+Toba-O@users.noreply.github.com>
2020-12-18 00:28:33 -0500

    Update INI2.py

--- INI2.py
+++ INI2.py
@@ -1,1 +1,19 @@
+# INI2 Variables and Some Arithmetic
 
+#Problem
+#Given: Two positive integers a and b, each less than 1000.
+#Return: The integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths a and b.
+  
+  
+  
+# Given: 978 828
+
+#Run
+>>> a = 978
+>>> b = 828
+>>> c_squared = a*a + b*b
+>>> print (c_squared)
+1642068
+
+#Result
+1642068
